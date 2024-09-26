938c7fdfb56961304da0b2fac48cb7651b4e5545
Aina Tiavina <rajoelisonainatiavina@gmail.com>
2024-09-26 18:35:18 +0300

    done:gradient descent

--- utils/linear_algebra_module.py
+++ utils/linear_algebra_module.py
@@ -12,3 +12,12 @@ assert dot([1, 2, 3], [4, 5, 6]) == 32
 def sum_of_squares(v: Vector) -> float:
     """Return v_1 * v_1 + ... + v_n * v_n"""
     return dot(v, v)
+
+def add(v: Vector, w: Vector) -> Vector:
+    """Add corresponding elements"""
+    assert len(v) == len(w), "Vector should be have the same length"
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+    
+def scalar_multiply(c: float, v: Vector) -> Vector:
+    """Multiply every element by c"""
+    return [c * v_i for v_i in v]
